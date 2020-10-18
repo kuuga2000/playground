60dd93f8df4a97c3296fe8b12172f316cd4bf038
Kuuga <ryuki.servaiv@gmail.com>
2020-10-18 21:15:38 +0700

    First Commit in React Playground

---
+++ webtcaer/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.[hash].js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+      new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+      new HtmlWebpackPlugin({
+          template: "./src/index.html",
+          title: "Happy Learning!"
+      }),
+    ],
+    resolve: {
+        modules: [__dirname, "src", "node_modules"],
+        extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        inline: true,
+        hot: true,
+        port: 8001
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: require.resolve("babel-loader"),
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.png|svg|jpg|gif$/,
+                use: ["file-loader"],
+            },
+        ],
+    },
+};
