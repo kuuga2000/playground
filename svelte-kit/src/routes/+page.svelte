6740df600788fd3009811b10c00acc664e75cc88
Uga <ryuki.servaiv@gmail.com>
2023-02-06 22:29:53 +0700

    component

---
+++ svelte-kit/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    let title ="Kamen Rider RX";
+    const updateTitle = () => {
+        title = "Kamen Rider Black RX"
+    }
+</script>
+<div id="index" class="index">
+    {title}
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi pariatur neque dolores nemo, cumque eius, ipsum numquam amet similique voluptate et sunt, odio deleniti provident. Maxime odit iste modi laboriosam.
+    </p>
+    <button on:click={updateTitle}>Change Title</button>
+</div>
+<style>
+    .index {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
