af2e7b8033c4e123fd7f829b8b6df0dbe909a330
Uga <syarif@sukamart.com>
2023-03-11 14:47:23 +0700

    reactive value

---
+++ svelte-vite/src/walkthrough/components/Event.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    let name = "Minami Kotaro";
+    let color = "red";
+    $: colorName = `${color} ${name}`
+    $: {
+        console.log('===')
+        console.log(colorName)
+    }
+    /*const textChange = (event) => {
+        name = event.target.value;
+    };*/
+</script>
+
+<p style="color: {color}">
+    My Name is {colorName}
+</p>
+<!--
+<input type="text" on:input={textChange} value={name} />
+-->
+<input type="text" bind:value={name}>
+<input type="text" bind:value={color}>
