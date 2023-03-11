ab61bc78970c63a96933e47a9bcd0f9be9160330
Uga <ryuki.servaiv@gmail.com>
2023-03-11 23:48:10 +0700

    CSS & Conditional Styles

--- svelte-vite/src/Loop.svelte
+++ svelte-vite/src/Loop.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Loop from './walkthrough/components/Loop.svelte'
+  import svelteLogo from "./assets/svelte.svg";
+  import Loop from "./walkthrough/components/Loop.svelte";
+  import Modal from "./walkthrough/components/Modal.svelte";
 </script>
 
 <main>
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
       <img src="/vite.svg" class="logo" alt="Vite Logo" />
     </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
   <h1>Vite + Svelte</h1>
-
   <div class="card">
     <Loop />
   </div>
 </main>
-
+<Modal />
 <style>
   .logo {
     height: 6em;
@@ -31,8 +31,5 @@
   }
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
   }
 </style>
