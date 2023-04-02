32808911c2e06deeb8effc6bd113f482361d44e1
Kuuga2000 <ryuki.servaiv@gmail.com>
2023-04-02 13:37:38 +0700

    note

--- react-vite/src/components/HeroList.jsx
+++ react-vite/src/components/HeroList.jsx
@@ -2,7 +2,7 @@ const HeroList = ({heroes, btnRemove}) => {
     return (
         <ul>
             {heroes.map((hero) => (
-                <li key={hero.id} style={{ listStyle: 'none' }}>
+                <li className="test abc" key={hero.id} style={{ listStyle: 'none' }}>
                     {hero.name} - <a href="#" onClick={() => {
                         btnRemove(hero.id)
                     }}>Remove</a>
