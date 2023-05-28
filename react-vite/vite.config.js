51046b4a308733ab9312160f2c829f87f6cfbf3f
ryuki.servaiv@gmail.com <ryuki.servaiv@gmail.com>
2023-05-29 00:01:15 +0700

    test

--- react-vite/vite.config.js
+++ react-vite/vite.config.js
@@ -1,11 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {viteObfuscateFile} from 'vite-plugin-obfuscator'
+import { viteObfuscateFile } from 'vite-plugin-obfuscator'
+import path from 'path';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   
+  build: {
+    rollupOptions: [
+      {
+        input: 'math.ts',
+        output: {
+          file: 'dist/a.js',
+          format: 'es'
+        }
+      }, {
+        input: 'logger.ts',
+        output: {
+          file: 'dist/ab.js',
+          format: 'es'
+        }
+      }
+    ],
+  },
   plugins: [
     react(),
     viteObfuscateFile()
-  ]
+  ],
 })
