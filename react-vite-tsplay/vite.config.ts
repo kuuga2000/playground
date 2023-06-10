9a51ab8e958df767bfbc00e7addcf11a26afb0d0
Kuuga2000 <--edit>
2023-06-10 23:57:30 +0700

    compile multiple folder output

--- react-vite-tsplay/vite.config.ts
+++ react-vite-tsplay/vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { viteObfuscateFile } from 'vite-plugin-obfuscator'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '',
+  root:'./',
+  build: {
+    emptyOutDir: false,
+    outDir: "pub",
+    sourcemap: false,
+    lib: {
+      formats: ["es"],
+      entry: {
+        'static/store/company/desktop/id_ID/company_product/product':"./src/product.tsx",
+        'static/store/company/desktop/id_ID/company_customer/customer':"./src/customer.tsx",
+      },
+    },
+  },
+  plugins: [
+    react(),
+    viteObfuscateFile()
+  ],
 })
