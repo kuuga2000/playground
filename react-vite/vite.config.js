73bc1c09eeacc331cc37f20312e44fa17fe0b499
Kuuga2000 <--edit>
2023-06-10 22:42:36 +0700

    compile multiple page

--- react-vite/vite.config.js
+++ react-vite/vite.config.js
@@ -1,28 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { viteObfuscateFile } from 'vite-plugin-obfuscator'
-import path from 'path';
-
+//import path from 'path';
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   
+  base: '',
+  root:'./',
   build: {
-    rollupOptions: [
-      {
-        input: 'math.ts',
-        output: {
-          file: 'dist/a.js',
-          format: 'es'
-        }
-      }, {
-        input: 'logger.ts',
-        output: {
-          file: 'dist/ab.js',
-          format: 'es'
-        }
-      }
-    ],
+    emptyOutDir: false,
+    outDir: "dist",
+    sourcemap: false,
+    lib: {
+      formats: ["es"],
+      entry: {
+        'test/mathx/test':"./src/math.ts",
+        'xx/a/loggerx':"./src/logger.ts",
+      },
+    },
   },
   plugins: [
     react(),
