ec45ba9b8d6664e99b124739ad3cc4ad35c49e98
Kuuga2000 <--edit>
2023-06-11 13:43:18 +0700

    variable play env

--- react-vite-tsplay/vite.config.ts
+++ react-vite-tsplay/vite.config.ts
@@ -1,21 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 //import.meta.env.VITE_APP_TITLE
- 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      // https://rollupjs.org/configuration-options/
-      input: {
-        'Customer/customer': 'src/customer.tsx',
-        'Product/product': 'src/product.tsx'
-      },
-      output: {
-        dir:'pub/static/store/company/desktop/id_ID',
-        entryFileNames: 'Company_[name].js',
-      }
-    }
-  },
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  console.log(env.default);
+  return {
+    // vite config
+    define: {
+      __APP_ENV__: env.APP_ENV,
+    },
+  }
 })
