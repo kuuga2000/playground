762ccf89a3879ffbd7784ae6bd6a2c2c2bc344c5
ryuki.servaiv@gmail.com <ryuki.servaiv@gmail.com>
2023-06-12 21:11:55 +0700

    splitVendorChunkPlugin

--- react-vite-tsplay/vite.config.default.ts
+++ react-vite-tsplay/vite.config.default.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { splitVendorChunkPlugin } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -22,6 +23,6 @@ export default defineConfig(({ command, mode }) => {
         }
       }
     },
-    plugins: [react()]
+    plugins: [splitVendorChunkPlugin()]
   }
 })
--- react-vite-tsplay/vite.config.ts
+++ react-vite-tsplay/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { splitVendorChunkPlugin } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  plugins: [splitVendorChunkPlugin()],
 })
